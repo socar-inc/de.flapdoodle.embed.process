aca100219da38891147e4200f04f5090fde75a64
Michael Mosmann <michael@mosmann.de>
2018-05-18 11:50:54 +0200

    disable test

--- src/test/java/de/flapdoodle/embed/process/howto/HowToBuildAProcessConfigTest.java
+++ src/test/java/de/flapdoodle/embed/process/howto/HowToBuildAProcessConfigTest.java
@@ -37,6 +37,7 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Optional;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import de.flapdoodle.embed.process.config.store.DistributionPackage;
@@ -63,82 +64,91 @@ import de.flapdoodle.types.Try;
 public class HowToBuildAProcessConfigTest {
 
 	@Test
+	@Ignore("it is an integration test")
 	public void simpleSample() {
 		NamedType<Path> artifactStore = typeOf("artifactStore", Path.class);
 		NamedType<Path> artifactPath = typeOf("artifactPath", Path.class);
 		NamedType<Path> downloadedArtifactPath = typeOf("downloadedArtifactPath", Path.class);
-		
+
 		InitRoutes<SingleDestination<?>> routes = InitRoutes.builder()
-			.add(Start.of(typeOf(Version.class)), () -> State.of(Version.of("2.1.1")))
-			.add(Start.of(artifactStore), () -> artifactStore())
-			.add(Start.of(typeOf(DownloadPath.class)), () -> State.of(DownloadPath.of("https://bitbucket.org/ariya/phantomjs/downloads/")))
-			.add(Bridge.of(typeOf(Version.class), typeOf(Distribution.class)), (version) -> State.of(Distribution.detectFor(version)))
-			.add(Bridge.of(typeOf(Distribution.class), typeOf(DistributionPackage.class)), (distribution) -> packageOf(distribution))
-			.add(MergingJunction.of(typeOf(DownloadPath.class), typeOf(DistributionPackage.class), typeOf(URL.class)), (downloadPath, dist) -> {
-				return downloadUrl(downloadPath, dist);
-			})
-			.add(MergingJunction.of(artifactStore, typeOf(DistributionPackage.class), artifactPath), (store, dist) -> artifactPath(store, dist))
-			.add(MergingJunction.of(artifactPath, typeOf(URL.class), downloadedArtifactPath), (path, url) -> useOrDownloadArtifact(path, url))
-			.build();
-		
+				.add(Start.of(typeOf(Version.class)), () -> State.of(Version.of("2.1.1")))
+				.add(Start.of(artifactStore), () -> artifactStore())
+				.add(Start.of(typeOf(DownloadPath.class)),
+						() -> State.of(DownloadPath.of("https://bitbucket.org/ariya/phantomjs/downloads/")))
+				.add(Bridge.of(typeOf(Version.class), typeOf(Distribution.class)),
+						(version) -> State.of(Distribution.detectFor(version)))
+				.add(Bridge.of(typeOf(Distribution.class), typeOf(DistributionPackage.class)),
+						(distribution) -> packageOf(distribution))
+				.add(MergingJunction.of(typeOf(DownloadPath.class), typeOf(DistributionPackage.class), typeOf(URL.class)),
+						(downloadPath, dist) -> {
+							return downloadUrl(downloadPath, dist);
+						})
+				.add(MergingJunction.of(artifactStore, typeOf(DistributionPackage.class), artifactPath),
+						(store, dist) -> artifactPath(store, dist))
+				.add(MergingJunction.of(artifactPath, typeOf(URL.class), downloadedArtifactPath),
+						(path, url) -> useOrDownloadArtifact(path, url))
+				.build();
+
 		try (Init<Path> initArtifactStore = InitLike.with(routes).init(artifactStore)) {
-			
+
 			try (Init<Path> init = initArtifactStore.init(downloadedArtifactPath)) {
-				System.out.println("current: "+init.current());
-				
+				System.out.println("current: " + init.current());
+
 			}
-			
+
 			try (Init<Path> init = initArtifactStore.init(downloadedArtifactPath)) {
-				System.out.println("current: "+init.current());
-				
+				System.out.println("current: " + init.current());
+
 			}
 		}
 	}
-	
+
 	private static State<Path> useOrDownloadArtifact(Path artifactPath, URL downloadUrl) {
 		if (!artifactPath.toFile().exists()) {
 			Try.supplier(() -> {
-				TimeoutConfig timeoutConfig=TimeoutConfig.defaults();
-				URLConnection connection = UrlStreams.urlConnectionOf(downloadUrl, "flapdoodle-user-agent", timeoutConfig, Optional.empty());
+				TimeoutConfig timeoutConfig = TimeoutConfig.defaults();
+				URLConnection connection = UrlStreams.urlConnectionOf(downloadUrl, "flapdoodle-user-agent", timeoutConfig,
+						Optional.empty());
 				UrlStreams.downloadTo(connection, artifactPath, listener());
 				return State.of(artifactPath);
 			})
-			.mapCheckedException(RuntimeException::new)
-			.get();
+					.mapCheckedException(RuntimeException::new)
+					.get();
 		}
 		return State.of(artifactPath);
 	}
-	
+
 	private static UrlStreams.DownloadCopyListener listener() {
 		return new DownloadCopyListener() {
 			long lastMessage = System.currentTimeMillis();
+
 			@Override
 			public void downloaded(long bytesCopied, long contentLength) {
 				long current = System.currentTimeMillis();
-				if (current-lastMessage>1000 || bytesCopied==contentLength) {
-					lastMessage=current;
-					if (contentLength>0) {
-						System.out.print(bytesCopied*100/contentLength+"% ");
+				if (current - lastMessage > 1000 || bytesCopied == contentLength) {
+					lastMessage = current;
+					if (contentLength > 0) {
+						System.out.print(bytesCopied * 100 / contentLength + "% ");
 					}
-					if (contentLength==bytesCopied) {
+					if (contentLength == bytesCopied) {
 						System.out.println();
 					}
 				}
 			}
 		};
 	}
-	
+
 	private static State<Path> artifactPath(Path storePath, DistributionPackage dist) {
 		return State.of(storePath.resolve(dist.archivePath()));
 	}
-	
+
 	private static State<Path> artifactStore() {
 		return Try.supplier(() -> {
-					Path artifactStore = Files.createTempDirectory("artifactStore-");
-					return State.of(artifactStore, (path) -> {
-						deleteDirectoryAndContent(path);
-					});
-				})
+			Path artifactStore = Files.createTempDirectory("artifactStore-");
+			return State.of(artifactStore, (path) -> {
+				deleteDirectoryAndContent(path);
+			});
+		})
 				.mapCheckedException(RuntimeException::new)
 				.get();
 	}
@@ -171,10 +181,9 @@ public class HowToBuildAProcessConfigTest {
 					Files.delete(dir);
 					return FileVisitResult.CONTINUE;
 				}
-				
+
 			});
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
@@ -182,36 +191,35 @@ public class HowToBuildAProcessConfigTest {
 	private static State<Path> downloadedArtifact() {
 		return State.of(Paths.get(""));
 	}
-	
+
 	private static State<URL> downloadUrl(DownloadPath path, DistributionPackage dist) {
 		try {
-			return State.of(new URL(path.value()+dist.archivePath()));
-		}
-		catch (MalformedURLException e) {
-			throw new RuntimeException("could not create downloadUrl for "+path+" and "+dist);
+			return State.of(new URL(path.value() + dist.archivePath()));
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("could not create downloadUrl for " + path + " and " + dist);
 		}
 	}
-	
+
 	private static State<DistributionPackage> packageOf(Distribution distribution) {
 		ArchiveType archiveType = getArchiveType(distribution);
 		return State.of(DistributionPackage.of(archiveType, fileSetFor(distribution), getPath(distribution, archiveType)));
 	}
-	
+
 	private static FileSet fileSetFor(Distribution distribution) {
 		String execName;
 		switch (distribution.platform()) {
 			case Windows:
-				execName="phantomjs.exe";
+				execName = "phantomjs.exe";
 				break;
 			default:
-				execName="phantomjs";
+				execName = "phantomjs";
 		}
-		
+
 		return FileSet.builder()
-				.addEntry(FileType.Executable,execName)
+				.addEntry(FileType.Executable, execName)
 				.build();
 	}
-	
+
 	private static ArchiveType getArchiveType(Distribution distribution) {
 		switch (distribution.platform()) {
 			case OS_X:
@@ -223,30 +231,31 @@ public class HowToBuildAProcessConfigTest {
 
 	private static String getPath(Distribution distribution, ArchiveType archiveType) {
 		final String packagePrefix;
-		String bitVersion="";
+		String bitVersion = "";
 		switch (distribution.platform()) {
 			case OS_X:
-				packagePrefix="macosx";
+				packagePrefix = "macosx";
 				break;
 			case Windows:
-				packagePrefix="windows";
+				packagePrefix = "windows";
 				break;
 			default:
-				packagePrefix="linux";
+				packagePrefix = "linux";
 				switch (distribution.bitsize()) {
 					case B64:
-						bitVersion="-x86_64";
+						bitVersion = "-x86_64";
 						break;
 					default:
-						bitVersion="-i686";
+						bitVersion = "-i686";
 				}
 		}
-		
-		String packageExtension=".zip";
-		if (archiveType==ArchiveType.TBZ2) {
-			packageExtension=".tar.bz2";
+
+		String packageExtension = ".zip";
+		if (archiveType == ArchiveType.TBZ2) {
+			packageExtension = ".tar.bz2";
 		}
-		return "phantomjs-"+distribution.version().asInDownloadPath()+"-"+packagePrefix+bitVersion+packageExtension;
+		return "phantomjs-" + distribution.version().asInDownloadPath() + "-" + packagePrefix + bitVersion
+				+ packageExtension;
 	}
 
 }
